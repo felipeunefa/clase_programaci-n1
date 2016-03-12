95ea2a39e0e3f3d73d089d5210912fc163d0f646
Juan Pablo Rodrigues <jpablio@Juventud-Productiva.(none)>
2016-03-11 20:21:40 -0430

    ejemplo 1 while y correcci ón de ortografia

--- felipeVillamizar/clase1/ejemploclase.js
+++ felipeVillamizar/clase1/ejemploclase.js
@@ -1,2 +1,12 @@
-alert('hola mundo')
-console.log('hola mundo')
+var m = parseInt(prompt("Introduzca un dato"));
+var n = parseInt(prompt("Introduzca otro dato")); 
+var p=0;
+var m_inicial=m
+while (m > 0) {
+    m=m-1;
+    p=p+n;
+    alert('El producto de '+m_inicial+' y '+n+' es: '+p)
+    
+} 
+console.log('El producto de '+m_inicial+' y '+n+' es: '+p)
+
