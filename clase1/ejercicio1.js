8150910337651d14fe88bba2bf7ef31ca651ab86
Javier_Montesinos <nxganja@gmail.com>
2016-03-12 00:19:34 -0430

    lo subi donde no era xd

--- clase1/ejercicio1.js
+++
@@ -1,9 +0,0 @@
-var cedula = 24223859;
-var mayor = 9;
-var menor = 2;
-if(mayor >= menor)
-{
-    alert('Tu numero de cedula es: '+cedula);
-    alert('El mayor es: '+mayor);
-    alert('el menor es: '+menor);
-}
