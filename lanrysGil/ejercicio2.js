080703fa631c98a3d283b9c7c3ecd960e0ec7d20
lanrys gil <lanryscifras@gmail.com>
2016-03-13 17:40:33 -0430

    mis ejercicios de programaciion

---
+++ lanrysGil/ejercicio2.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+;
+var x = parseInt(prompt('ingrese un valor entero para x'));
+
+var y = parseInt(prompt('ingrese un valor entero para y'));
+
+if(x >= y)
+{
+  alert('x es mayor o igual que y');  
+}else if(x <= y)
+{
+    alert('x es menor o igual que y');
+}
+else
+{
+    alert('x es igual a cero');
+}
