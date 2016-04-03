299a5d8e54d52c013615476af723e4edca526da0
Allan <asllanrc@gmail.com>
2016-04-03 18:44:43 -0430

    Se agregaron los archivos de la base de datos con y los archivos de Vista y Modelo.

---
+++ AllanCastro/Registro_Equipos/config.php
@@ -0,0 +1,7 @@
+<?php 
+    define('DB_HOST','localhost'); 
+    define('DB_USER','root'); 
+    define('DB_PASS',''); 
+    define('DB_NAME','Registro_Equipos'); 
+    define('DB_CHARSET','utf-8'); 
+?> 
