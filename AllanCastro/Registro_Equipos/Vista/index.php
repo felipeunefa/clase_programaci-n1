937d040a461defbfbc4877a45795216d2cb88fbe
Allan <asllanrc@gmail.com>
2016-04-19 15:26:02 -0430

    Se agregaron los paquetes de BootsTrap y JQuery, además se utilizaron los archivos de vista y Modelo.

--- AllanCastro/Registro_Equipos/Vista/index.php
+++ AllanCastro/Registro_Equipos/Vista/index.php
@@ -1,52 +1,48 @@
 <?php   
      require_once "../Modelo/equipos.php"; 
-
-    $usuarioModel = new equiposM(); 
-    $a_users = $usuarioModel->get_equipos(); 
+     require_once "../Modelo/Estructura.php"; 
+    $equiposObj = new equiposM(); 
+    $estructura = new Estructura(); 
 ?> 
 
 <!DOCTYPE html> 
  <html> 
  <head> 
-     <title>Usuarios registrados</title> 
+     <title>Equipos registrados</title>
+     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+     <?php 
+          $estructura->get_biblotecas()
+         ?>
  </head> 
- <body> 
-     <table > 
-            <tr> 
-                <td> 
-                    Codigo 
-                </td> 
-                <td > 
-                    Marca 
-                </td> 
-                <td> 
-                    Modelo 
-                </td>
-		<td>
-		    Serie
-		</td>
-		<td>
-		    Monitor
-		</td>
-		<td>
-		    Impresora
-		</td>
-            </tr><!-- /THEAD --> 
+ <body
+     <div class="container">
+         <?php 
+          $estructura->get_menu()
+         ?>
 
-            <?php foreach ($a_users as $row): ?> 
-
-            <tr> 
-                <td><?php echo $row['id_computadora']; ?></td> 
-                <td><?php echo $row['id_marca']; ?></td> 
-                <td><?php echo $row['modelo']; ?></td>
-		<td><?php echo $row['serie']; ?></td>
-		<td><?php echo $row['id_monitor']; ?></td>
-		<td><?php echo $row['id_impresora']; ?></td> 
-            </tr><!-- /TROW --> 
-         
-            <?php endforeach ?>     
-                  
-        </table> 
+        <div class="row">
+            <div class="col-md-2">
+                <?php 
+                $estructura->get_logo("")
+            ?>
+            </div>
+            <div class="col-md-10">
+                <div class="jumbotron">
+                    <h1 align="center">Ejemplo de un CRUD</h1>
+        <p align="center">Este es un ejemplo de un CRUD de registro de equipos, para que los alumnos lo tomen como referencia para su poyecto de fin de semestre.</p>
+        <?php $equiposObj->get_equipos();?>
+        <a class="btn btn-lg btn-primary" href="//github.com/allanrc23/clase_programaci-n1.git" role="button"><span class="glyphicon glyphicon-check"></span>Repositorio en Github</a>
+                </div>
+            </div>
+        </div>
+    </div>
+     
+     <?php 
+        $estructura->get_footer()
+    ?>
+    
     
  </body> 
- </html> 
+ </html>  
