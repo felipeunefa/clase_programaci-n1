683a0472a6915aa81a8d653895c901bcafb64108
isaacLaplante <isajhon79@gmail.com>
2016-04-26 11:32:42 -0400

    actualizando

---
+++ isaacLaplante/peluqueria2/Modelo/corte.php
@@ -0,0 +1,22 @@
+<?php
+require_once "conexion.php"; 
+class corte extends Conexion {
+    
+    public function get_select(){
+        $result = $this->_db->query('select * from corte where enlace=1'); 
+        
+		$corteData = $result->fetch_all(MYSQLI_ASSOC);
+        $select='<div class="form-group">
+                    <label for="sel1">Corte:</label>
+                    <div class="input-group col-xs-9">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-scissors"></span></span>
+                  <select class="form-control" id="sel1">
+                  <option >Seleccione..</option>';
+        foreach ($corteData as $corte):
+            $select=$select. '<option value="'.$corte['id_corte'].'">'.$corte['descripcion'].'</option>';
+        endforeach;
+    $select=$select. '</select></div></div>';
+    echo $select;
+    }
+} 
+    
